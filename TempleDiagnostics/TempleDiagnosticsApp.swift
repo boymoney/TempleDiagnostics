77f3e5169d3c4b03c86a6ddeda84053ee209e90d
David <davidcarnivale@gmail.com>
2024-07-06 02:30:11 -0400

    Initial Commit

---
+++ TempleDiagnostics/TempleDiagnosticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempleDiagnosticsApp.swift
+//  TempleDiagnostics
+//
+//  Created by David Carnivale on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TempleDiagnosticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
