7643c44de808cb2a6f3bc68489d33b3e86dd47cc
David <davidcarnivale@gmail.com>
2024-07-07 18:12:30 -0400

    Update project structure and remove unnecessary files

--- TempleDiagnostics/CameraControllerDelegate.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import CoreGraphics
-
-protocol CameraControllerDelegate: AnyObject {
-    func didRecognizeText(_ text: String, boundingBox: CGRect, score: Double)
-    func didFailToRecognizeText(with error: Error)
-}
